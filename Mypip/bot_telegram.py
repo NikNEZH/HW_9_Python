397a3ad1ff30575d608ece6711d0b45fb5e500db
NIKITA <kit.zaver@yandex.ru>
2023-02-04 21:19:22 +0300

    lol

--- Mypip/bot_telegram.py
+++ Mypip/bot_telegram.py
@@ -1,9 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import bot_comits as bc
+import tok as t
 
-
-app = ApplicationBuilder().token("6069521139:AAH5lLaVp7qt5YYWACUdfGUP6TGr39m55mI").build()
+app = ApplicationBuilder().token(t.token).build()
 
 app.add_handler(CommandHandler("hello", bc.hello))
 app.add_handler(CommandHandler("time", bc.time))
